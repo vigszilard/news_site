b3ade2631a3cdf5e6b0ca4898fb52a341ea3f877
Rafaela Georgescu <rafaela.georgescu@accesa.eu>
2023-12-12 09:33:25 +0200

    added filter

--- index.php
+++ index.php
@@ -4,13 +4,25 @@
     include "classes/Auth.php";
     include "classes/Journalist.php";
     include "classes/Article.php";
+    include "classes/Category.php";
 
     session_start();
     $database = new Database();
     $journalists = new Journalist($database);
     $authorization = new Auth($journalists);
     $article = new Article($database);
+    $category = new Category($database);
     $all_articles = $article -> get_approved_articles();
+    $all_categories = $category -> get_all_categories();
+
+    $articles_by_category = [];
+    foreach ($all_articles as $article) {
+        $category_id = $article["category_id"];
+        if (!isset($articles_by_category[$category_id])) {
+            $articles_by_category[$category_id] = [];
+        }
+        $articles_by_category[$category_id][] = $article;
+    }
 ?>
 
 
@@ -29,58 +41,128 @@
     <body>
         <?php include "components/header.php"; ?>
         <?php include "components/error_toast.php"; ?>
+        <div class="container mt-5 mb-5 text-center">
+            <ul class="nav nav-tabs mb-4" id="myTabs">
+                <li class="nav-item">
+                    <a class="nav-link active text-dark" id="all-tab" data-toggle="tab" href="#all">All Articles</a>
+                </li>
+                <?php foreach ($all_categories as $category): ?>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" id="category-<?php echo $category['id']; ?>-tab" data-toggle="tab" href="#category-<?php echo $category['id']; ?>">
+                            <?php echo $category['name']; ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
 
-        <div class="container mt-5 mb-5">
-            <div class="row justify-content-center">
-
-                <?php if (empty($all_articles)): ?>
-                    <div class="col-md-6 text-center">
-                        <p>No articles available</p>
-                    </div>
-                <?php else: ?>
-                    <?php foreach ($all_articles as $article): ?>
-                        <div class="col-md-4 mb-4">
-                            <div class="card h-100">
-                                <div class="card-body text-center d-flex flex-column">
-                                    <h5 class="card-title">
-                                        <?php echo $article["title"]; ?>
-                                    </h5>
-                                    <div class="d-flex justify-content-between">
+            <div class="tab-content">
+                <div class="tab-pane fade show active" id="all">
+                    <div class="row justify-content-center">
+                        <?php if (empty($all_articles)): ?>
+                            <div class="col-md-6 text-center">
+                                <p>No articles available</p>
+                            </div>
+                        <?php else: ?>
+                            <?php foreach ($all_articles as $article): ?>
+                                <div class="col-md-4 mb-4">
+                                    <div class="card h-100">
+                                        <div class="card-body text-center d-flex flex-column">
+                                            <h5 class="card-title">
+                                                <?php echo $article["title"]; ?>
+                                            </h5>
+                                            <div class="d-flex justify-content-between">
                                         <span class="card-subtitle mb-2 text-muted font-italic py-2">
                                             <?php
                                             $journalist = $journalists -> get_journalist_by_id($article["author_id"]);
                                             echo $journalist["firstname"] . " " . $journalist["lastname"];
                                             ?>
                                         </span>
-                                        <span class="card-subtitle mb-2 text-muted font-italic py-2">
+                                                <span class="card-subtitle mb-2 text-muted font-italic py-2">
                                             <?php
-                                                try {
-                                                    echo format_timestamp($article["created_at"]);
-                                                } catch (Exception $e) {
-                                                    echo "Unknown";
-                                                }
+                                            try {
+                                                echo format_timestamp($article["created_at"]);
+                                            } catch (Exception $e) {
+                                                echo "Unknown";
+                                            }
                                             ?>
                                         </span>
+                                            </div>
+                                            <p class="card-text flex-grow-1 text-justify">
+                                                <?php echo get_substring($article["content"]); ?>
+                                            </p>
+
+                                            <?php
+                                            $_SESSION["article_id"] = $article["id"];
+                                            $is_logged_in = isset($_SESSION["user"]);
+                                            $modal_attributes = $is_logged_in ? "" : "data-toggle='modal' data-target='#readMoreModal'";
+                                            $button_action = $is_logged_in ? "location.href='article.php?id={$article['id']}'" : "setArticleId('{$article['id']}')";
+                                            ?>
+                                            <button type="button" class="btn btn-primary mt-auto" <?php echo $modal_attributes; ?> onclick="<?php echo $button_action; ?>">
+                                                Read More
+                                            </button>
+
+                                        </div>
                                     </div>
-                                    <p class="card-text flex-grow-1 text-justify">
-                                        <?php echo get_substring($article["content"]); ?>
-                                    </p>
-
-                                    <?php
-                                        $_SESSION["article_id"] = $article["id"];
-                                        $is_logged_in = isset($_SESSION["user"]);
-                                        $modal_attributes = $is_logged_in ? "" : "data-toggle='modal' data-target='#readMoreModal'";
-                                        $button_action = $is_logged_in ? "location.href='article.php?id={$article['id']}'" : "setArticleId('{$article['id']}')";
-                                    ?>
-                                    <button type="button" class="btn btn-primary mt-auto" <?php echo $modal_attributes; ?> onclick="<?php echo $button_action; ?>">
-                                        Read More
-                                    </button>
-
                                 </div>
-                            </div>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+
+                <?php foreach ($all_categories as $category): ?>
+                    <div class="tab-pane fade" id="category-<?php echo $category['id']; ?>">
+                        <div class="row justify-content-center">
+                            <?php if (isset($articles_by_category[$category['id']])): ?>
+                                <?php foreach ($articles_by_category[$category['id']] as $article): ?>
+                                    <div class="col-md-4 mb-4">
+                                        <div class="card h-100">
+                                            <div class="card-body text-center d-flex flex-column">
+                                                <h5 class="card-title">
+                                                    <?php echo $article["title"]; ?>
+                                                </h5>
+                                                <div class="d-flex justify-content-between">
+                                        <span class="card-subtitle mb-2 text-muted font-italic py-2">
+                                            <?php
+                                            $journalist = $journalists -> get_journalist_by_id($article["author_id"]);
+                                            echo $journalist["firstname"] . " " . $journalist["lastname"];
+                                            ?>
+                                        </span>
+                                                    <span class="card-subtitle mb-2 text-muted font-italic py-2">
+                                            <?php
+                                            try {
+                                                echo format_timestamp($article["created_at"]);
+                                            } catch (Exception $e) {
+                                                echo "Unknown";
+                                            }
+                                            ?>
+                                        </span>
+                                                </div>
+                                                <p class="card-text flex-grow-1 text-justify">
+                                                    <?php echo get_substring($article["content"]); ?>
+                                                </p>
+
+                                                <?php
+                                                $_SESSION["article_id"] = $article["id"];
+                                                $is_logged_in = isset($_SESSION["user"]);
+                                                $modal_attributes = $is_logged_in ? "" : "data-toggle='modal' data-target='#readMoreModal'";
+                                                $button_action = $is_logged_in ? "location.href='article.php?id={$article['id']}'" : "setArticleId('{$article['id']}')";
+                                                ?>
+                                                <button type="button" class="btn btn-primary mt-auto" <?php echo $modal_attributes; ?> onclick="<?php echo $button_action; ?>">
+                                                    Read More
+                                                </button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <div class="col-md-6 text-center">
+                                    <p>No articles in this category</p>
+                                </div>
+                            <?php endif; ?>
                         </div>
-                    <?php endforeach; ?>
-                <?php endif; ?>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
 
